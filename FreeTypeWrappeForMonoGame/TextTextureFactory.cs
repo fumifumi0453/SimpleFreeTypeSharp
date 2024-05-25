649b79a3e081426385954e745e88b5c2395ba6d3
fumifumi0453 <kusunoki1988@gmail.com>
2024-05-25 13:21:42 +0900

    フォントファイルをbyte配列データとして登録可能に対応

--- FreeTypeWrappeForMonoGame/TextTextureFactory.cs
+++ FreeTypeWrappeForMonoGame/TextTextureFactory.cs
@@ -28,6 +28,24 @@ namespace FreeTypeWrapper
             _Service.Add(fontKey, font);
             return true;
         }
+        public static bool LoadFont(string fontKey, byte[] bytes, float size)
+        {
+            if (_Service.ContainsKey(fontKey)) return false;
+
+            var font = new ImageFont();
+            try
+            {
+                font.SetFont(bytes);
+                font.SetSize(size);
+            }
+            catch
+            {
+                return false;
+            }
+
+            _Service.Add(fontKey, font);
+            return true;
+        }
 
         public static TextTexture Create(string fontKey, string text, Color color)
         {
