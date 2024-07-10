06db374fad8e25fa43c6115a9fcfcc499a2c8507
fumifumi0453 <kusunoki1988@gmail.com>
2024-07-10 11:49:59 +0900

    MonoGame用のテストプロジェクトを追加

---
+++ MonoTest/Game1.cs
@@ -0,0 +1,63 @@
+﻿using FreeTypeWrapper;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoTest
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        TextTexture textTexture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            TextTextureFactory.GraphicsDevice = this.GraphicsDevice;
+            TextTextureFactory.LoadFont("Default", @"JF-Dot-MPlusH12.ttf", 24);
+            textTexture = TextTextureFactory.Create("Default", "日本語でおｋ\r\nそう日本語でおｋ", Color.Violet);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(textTexture.GetTexture(), new Vector2(10, 40), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoTest/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MonoTest.Game1();
+game.Run();
